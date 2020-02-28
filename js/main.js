96600828ddc974f1cf3e78ae53f859526e42941a
lehel.balint <lehel.balint@pctrade.hu>
2020-02-28 15:51:42 +0100

    Navbar_withoutJquery

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    console.log("ready!");
+});
+/* $(document).ready(function (){    
+    let nav=$('.nav');
+    let toggleCollapse=$('.toggle-collapse');
+
+ 
+    toggleCollapse.click(function(){
+        nav.toggleClass('collapse');
+    });
+
+}); */
